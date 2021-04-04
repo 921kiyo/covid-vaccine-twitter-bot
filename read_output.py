23a2a2068b1a4e42ec890e17bac6a3fd662cb890
921kiyo <kiyo921@hotmail.co.jp>
2021-04-04 14:52:59 +0100

    update

--- read_output.py
+++ read_output.py
@@ -6,7 +6,7 @@ def _get_total_number():
     with open("output.txt", encoding="utf8") as f:
         for line in f:
             if found == 2:
-                return str(line.strip())
+                return ≈line.strip()
             if found == 1:
                 found += 1
             if "<td> 合計</td>" in line.strip():
@@ -15,7 +15,7 @@ def _get_total_number():
 
 def extract_total_num():
     line_with_num = _get_total_number()
-    num_list = re.findall(r"\d+", line_with_num)
+    num_list = re.findall(r"\d+", str(line_with_num))
 
     actual_num = "".join(str(elem) for elem in num_list)
     return int(actual_num)
