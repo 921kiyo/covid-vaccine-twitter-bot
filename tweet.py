6cfebf776c832cfa03a822b387da30a0603d560d
Kiyo Kunii <kiyo921@hotmail.co.jp>
2021-05-16 18:10:05 +0100

    update

--- tweet.py
+++ tweet.py
@@ -3,13 +3,6 @@ import tweepy
 from scrape import extract_total_num
 import os
 
-# access_token = "1378443057620729859-q0hwpZdfDtdCwQP3PSaGNKEuyIhab0"
-# access_token_secret = "trrEROXZUaBkwkc8VfxPR4OnYGtRsqL8u9Y82iNgsUOS6"
-
-# consumer_key = "eBiOzvAcDcz9vWqIO9IxBk94G"
-# consumer_secret = "Giz20R0HhtF8WxnNANYzH813Og1dcuf47uaCPBUH2twCnqQUWg"
-
-
 access_token = os.environ["TWITTER_ACCESS_TOKEN"]
 access_token_secret = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
 
@@ -60,4 +53,4 @@ tweet += "#新型コロナワクチン #CovidVaccines #covidjapan "
 tweet += f"{collect_date} "
 print(tweet)
 # Create a tweet
-# api.update_status(tweet)
+api.update_status(tweet)
