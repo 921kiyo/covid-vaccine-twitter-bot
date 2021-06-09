190ab280f88c21126f49af7a8e7b9bf189340bc7
921kiyo <kiyo921@hotmail.co.jp>
2021-06-10 00:14:48 +0100

    fixed

--- tweet.py
+++ tweet.py
@@ -2,9 +2,6 @@
 import tweepy
 from scrape import extract_total_num
 import os
-import locale
-
-locale.setlocale(locale.LC_ALL, "")
 
 access_token = os.environ["TWITTER_ACCESS_TOKEN"]
 access_token_secret = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
@@ -46,11 +43,11 @@ bar, ratio = get_progress_bar(people_vaccinated)
 
 tweet = bar
 tweet += f" {round(ratio, 1)}% "
-tweet += f"(1回目接種回数, vaccinated {people_vaccinated:n}名, +{people_vaccinated_trend:n})\n"
+tweet += f"(1回目接種回数, vaccinated {people_vaccinated:,}名, +{people_vaccinated_trend:,})\n"
 bar_full, ratio_full = get_progress_bar(people_fully_vaccinated)
 tweet += bar_full
 tweet += f" {round(ratio_full, 1)}% "
-tweet += f"(2回目接種回数, fully vaccinated {people_fully_vaccinated:n}名, +{people_fully_vaccinated_trend:n})\n"
+tweet += f"(2回目接種回数, fully vaccinated {people_fully_vaccinated:,}名, +{people_fully_vaccinated_trend:,})\n"
 tweet += "#新型コロナワクチン #CovidVaccines #covidjapan "
 tweet += f"{collect_date} "
 print(tweet)
